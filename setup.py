2bf1ae75552579aeb81a68f3167994241e374399
Gary Reynolds <gary@touch.asn.au>
2011-11-08 22:50:45 +1100

    Fix error in setup.py
    Seems when copying setup.py from another project I have propagated my
    typo to a few places!

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     author = 'Gary Reynolds',
     author_email = 'gary@touch.asn.au',
     description = 'Client for the Syra API.',
-    install_required = ['suds', 'python-dateutil'],
+    install_requires = ['suds', 'python-dateutil'],
     packages = ['syra'],
 )
