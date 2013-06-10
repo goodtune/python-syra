4086f94b3dfd73ae026a8a852211a73cbe05be16
Gary Reynolds <gary@touch.asn.au>
2013-06-10 17:07:29 +1000

    Remove Python 2.5 from the compatibility matrix. Add trove identifiers.

--- setup.py
+++ setup.py
@@ -12,4 +12,15 @@ setup(
     description = 'Client for the Syra API.',
     install_requires = filter(None, map(strip, open('requirements.txt'))),
     packages = ['syra'],
+    classifiers = [
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: Public Domain',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python',
+        'Topic :: Internet :: Name Service (DNS)',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+   ],
 )
