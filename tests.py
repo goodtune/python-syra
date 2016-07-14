08904f107dc2ac05d3d137a21e00a0634b40323c
Gary Reynolds <gary@touch.asn.au>
2016-07-14 17:45:14 +1000

    Remove test that does not exercise the API

--- tests.py
+++ tests.py
@@ -11,12 +11,6 @@ class SimpleTest(TestCase):
     def setUp(self):
         self.api = syra.TestAPI(timeout=3)
 
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
-
     def test_balance_string(self):
         balance = self.api.balance(as_decimal=False)
         self.assertEqual(balance, '$5.72 AUD')
