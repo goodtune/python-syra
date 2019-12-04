3ea40bb65d0af9ba0bdedebed1e7064a3eb541d2
Gary Reynolds <gary.reynolds@touchtechnology.com.au>
2019-12-05 08:45:39 +1100

    Fix domain renew output bug.

--- syra/cli.py
+++ syra/cli.py
@@ -137,7 +137,7 @@ def domain_renew(ctx, domain, period):
     ok = res["Status"] == "Registered"
     click.secho(u"\u2713 " if ok else u"\u2717 ", nl=False, fg="green" if ok else "red")
     click.secho(domain + " ")
-    click.secho(res["Expiry"], fg="yellow", bold=True)
+    click.secho(res["Expiry"].strftime("%d-%b-%Y"), fg="yellow", bold=True)
 
 
 def main():
